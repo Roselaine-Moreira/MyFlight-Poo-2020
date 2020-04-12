0c7ab45febdde1c6a3469f9168ced75007b9f324
roselaine <roselainemoreira3@gmail.com>
2020-04-12 01:26:54 -0300

    Adicionado método "listarTodos" em GerenciadorCias para teste.

--- src/com/company/GerenciadorCias.java
+++ src/com/company/GerenciadorCias.java
@@ -5,27 +5,31 @@ import java.util.ArrayList;
 public class GerenciadorCias {
     private ArrayList<CiaAerea> empresas;
 
-    private GerenciadorCias(){
+    private GerenciadorCias() {
         empresas = new ArrayList<>();
     }
 
     private static GerenciadorCias instance;
 
-    public static GerenciadorCias getGerenciadorCias(){
-        if(instance == null){
+    public static GerenciadorCias getGerenciadorCias() {
+        if (instance == null) {
             instance = new GerenciadorCias();
         }
         return instance;
     }
 
-    public void adicionar(String umCodigo, String umNome){
+    public ArrayList<CiaAerea> listarTodas() {
+        return new ArrayList<>(empresas);
+    }
+
+    public void adicionar(String umCodigo, String umNome) {
         empresas.add(new CiaAerea(umCodigo, umNome));
     }
 
     @Override
     public String toString() {
         String aux = "\nGerenciador de Cias Aéreas\n";
-        for(CiaAerea cia : empresas){
+        for (CiaAerea cia : empresas) {
             aux = aux + cia.toString() + "\n";
         }
         return aux;
