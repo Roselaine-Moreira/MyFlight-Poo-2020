fba321decb107e103efe12a7f356eb3d6c70621c
Jonatasvangroll <jonatas.vangroll@gmail.com>
2020-06-10 17:42:37 -0300

    implementado camparable
    adcionado a comparação

--- src/com/company/Aeroporto.java
+++ src/com/company/Aeroporto.java
@@ -1,33 +1,36 @@
 package com.company;
 
 public class Aeroporto {
+    
     private String codigo;
-    private String nome;
-    private Geo loc;
-
-    public Aeroporto(String umCodigo, String umNome, Geo umLoc) {
-        codigo = umCodigo;
-        nome = umNome;
-        loc = umLoc;
-    }
-    public String getCodigo() {
-        return codigo;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public Geo getLocal() {
-        return loc;
-    }
-
-    @Override
+	private String nome;
+	private Geo loc;
+	
+	public Aeroporto(String codigo, String nome, Geo loc) {
+		this.codigo = codigo;
+		this.nome = nome;
+		this.loc = loc;
+	}
+	
+	public String getCodigo() {
+		return codigo;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public Geo getLocal() {
+		return loc;
+	}
+	
+	@Override
     public String toString() {
-        return "Código do Aeroporto: " + codigo + " " +
-                "Nome do Aeroporto: " + nome + " " +
-                "Localização: " + loc + " ";
+        return codigo + " - " + nome + " [" + loc + "]";
     }
 
-
+	@Override
+	public int compareTo(Aeroporto outro) {
+		return this.nome.compareTo(outro.nome);
+	}
 }
